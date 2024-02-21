484d25500cc6be0b8e2e94aead021d24e73c1518
Jasmine Ampofo <akubahjazz@gmail.com>
2024-02-21 23:02:52 +0000

    fixed connection to MongoDB Atlas

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 const mongoose = require("mongoose");
-const morgan = require("morgan")
+const morgan = require("morgan");
 require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const UsersRoute = require("./routes/User.route");
 const AdminRoute = require("./routes/Admin.route");
 const SettingsRoute = require("./routes/Settings.route");
@@ -16,10 +16,10 @@ const SubscriptionPackageRoute = require("./routes/SubscriptionPackage.route");
 const SubscriptionRoute = require("./routes/Subscription.route");
 const PaymentRoute = require("./routes/Payment.route");
 const ExerciseRoute = require("./routes/Exercise.route");
-const TemplateRoute = require("./routes/Template.route")
+const TemplateRoute = require("./routes/Template.route");
 const AppointmentRoute = require("./routes/Appointment.route");
-const SecurityManager = require('./controllers/SecurityManager.controller')
-const bodyParser = require('body-parser');
+const SecurityManager = require("./controllers/SecurityManager.controller");
+const bodyParser = require("body-parser");
 const config = require("./config");
 
 const StorageManager = require("./utils/Storage.manager");
@@ -31,85 +31,109 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true, ssl: true };//ssl: process.env.NODE_ENV === "prod" ? true : false};
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+}; //ssl: process.env.NODE_ENV === "prod" ? true : false};
 
 if (process.env.NODE_ENV !== "test") {
-    mongoose.set('strictQuery', false);
-    mongoose.connect(config.DB_URL,
-        dbOptions,
-        (err) => {
-            if (err)
-                console.log(err)
-            else {
-                console.log("mongdb is connected");
-                InstallationManager.initializeSetup();
+  //   console.log(config);
+  mongoose.set("strictQuery", false);
+  mongoose.connect(config.DB_URL, dbOptions, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("mongdb is connected");
+      InstallationManager.initializeSetup();
 
-                // Register Automated tasks
-                console.log("set up automated tasks");
-                const automationManager = new AutomationManager();
-                const appointmentService = new AppointmentService();
-                automationManager.registerTask(30, appointmentService.rejectOverdueAppointments());
+      // Register Automated tasks
+      console.log("set up automated tasks");
+      const automationManager = new AutomationManager();
+      const appointmentService = new AppointmentService();
+      automationManager.registerTask(
+        30,
+        appointmentService.rejectOverdueAppointments()
+      );
 
-                // Start tasks
-                automationManager.initilizeTasks();
-                console.log("start tasks")
-            }
-        }
-    );
-    app.use(morgan('mmr - [:date[clf]] :remote-addr - :remote-user  ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"')); // common | combined
+      // Start tasks
+      automationManager.initilizeTasks();
+      console.log("start tasks");
+    }
+  });
+  app.use(
+    morgan(
+      'mmr - [:date[clf]] :remote-addr - :remote-user  ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
+    )
+  ); // common | combined
 }
 
-app.use('/', SecurityManager.check_allowed_requests);
+app.use("/", SecurityManager.check_allowed_requests);
 app.get("/file/:key", async (req, res) => {
-    const { key } = req.params;
-    if (!key) {
-        res.status(404).send("File not found");
-    }
-    const url = await new StorageManager().getFileUrl(key);
-    res.status(200).redirect(url);
-})
+  const { key } = req.params;
+  if (!key) {
+    res.status(404).send("File not found");
+  }
+  const url = await new StorageManager().getFileUrl(key);
+  res.status(200).redirect(url);
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create necesary configurations for application
-app.use('/api/exercises', ExerciseRoute);
-app.use('/api/appointments', AppointmentRoute);
-app.use('/api/sessions', SessionRoute);
-app.use('/api/bodyParts', BodyPartRoute);
-app.use('/api/healthProfessionalTypes', HealthProfessionalTypeRoute);
-app.use('/api/users', UsersRoute);
-app.use('/api/subscriptionPackages', SubscriptionPackageRoute);
-app.use('/api/subscriptions', SubscriptionRoute);
-app.use('/api/payments', PaymentRoute);
-app.use('/api/templates', TemplateRoute);
-app.use('/api/admins', AdminRoute);
-app.use('/api/settings', SettingsRoute);
-app.use('/api/storage/', StorageRoute);
-
+app.use("/api/exercises", ExerciseRoute);
+app.use("/api/appointments", AppointmentRoute);
+app.use("/api/sessions", SessionRoute);
+app.use("/api/bodyParts", BodyPartRoute);
+app.use("/api/healthProfessionalTypes", HealthProfessionalTypeRoute);
+app.use("/api/users", UsersRoute);
+app.use("/api/subscriptionPackages", SubscriptionPackageRoute);
+app.use("/api/subscriptions", SubscriptionRoute);
+app.use("/api/payments", PaymentRoute);
+app.use("/api/admins", AdminRoute);
+app.use("/api/settings", SettingsRoute);
+app.use("/api/storage/", StorageRoute);
+app.use("/api/templates", TemplateRoute);
 
 app.get("/file/:key", async (req, res) => {
-    const { key } = req.params;
-    if (!key) {
-        res.status(404).send("File not found");
-    }
-    const url = await new StorageManager().getFileUrl(key);
-    res.status(200).redirect(url);
-})
+  const { key } = req.params;
+  if (!key) {
+    res.status(404).send("File not found");
+  }
+  const url = await new StorageManager().getFileUrl(key);
+  res.status(200).redirect(url);
+});
 
 // Welcome message
-app.get('/', (req, res) => { res.json({ route: "MonitorMyRehab API", message: "Hello there. Are you on aws or where are you?\nThis is the monitorMyRehab API version 1.0.0" }); });
+app.get("/", (req, res) => {
+  res.json({
+    route: "MonitorMyRehab API",
+    message:
+      "Hello there. Are you on aws or where are you?\nThis is the monitorMyRehab API version 1.0.0",
+  });
+});
 
-app.post("*", (req, res) => { res.status(404).json({ message: 'Endpoint not supported', status: 404 }); })
-app.put("*", (req, res) => { res.status(404).json({ message: 'Endpoint not supported', status: 404 }); })
-app.delete("*", (req, res) => { res.status(404).json({ message: 'Endpoint not supported', status: 404 }); })
+app.post("*", (req, res) => {
+  res.status(404).json({ message: "Endpoint not supported", status: 404 });
+});
+app.put("*", (req, res) => {
+  res.status(404).json({ message: "Endpoint not supported", status: 404 });
+});
+app.delete("*", (req, res) => {
+  res.status(404).json({ message: "Endpoint not supported", status: 404 });
+});
 
 //const PORT = process.env.NODE_ENV === "test" ? Number.parseInt(config.PORT) + 1 : config.PORT;
 
 if (process.env.NODE_ENV === "dev") {
-    app.listen(5000, () => { console.log("Listening on port 5000") })
+  app.listen(5001, () => {
+    console.log("Listening on port 5001");
+  });
 }
 if (process.env.NODE_ENV !== "test") {
-    module.exports.handler = serverless(app);
+  console.log("serverless app exported");
+  module.exports.handler = serverless(app);
 } else {
-    module.exports = app;
+  console.log("Just app exported");
+  module.exports = app;
 }
+// mongodb+srv://mmr-dev:Dd9IJn63o0nQOrBh@mmr-test.lhe7vep.mongodb.net/?retryWrites=true
--- tests/integration/exercisetemplate.test.js
+++ tests/integration/exercisetemplate.test.js
@@ -1,27 +1,24 @@
-const request = require('supertest');
-const app = require('../../app'); // Update the path based on your project structure
-const db = require('../db');
-const env = require('../../config');
+const request = require("supertest");
+const app = require("../../app"); // Update the path based on your project structure
+const db = require("../db");
+const env = require("../../config");
 
 const agent = request.agent(app);
 
-const AdminService = require('../../services/Admin.service');
-const AuthenticationManager = require('../../utils/Authentication.manager');
-const TemplateService = require('../../services/Template.service');
-const ExerciseService = require('../../services/Exercise.service'); // Assuming you have Exercise service
-const BodyPartService = require('../../services/BodyPart.service');
-const UserService = require('../../services/User.service')
-const HealthProfessionalTypeService = require('../../services/HealthProfessionalType.service');
-
+const AdminService = require("../../services/Admin.service");
+const AuthenticationManager = require("../../utils/Authentication.manager");
+const TemplateService = require("../../services/Template.service");
+const ExerciseService = require("../../services/Exercise.service"); // Assuming you have Exercise service
+const BodyPartService = require("../../services/BodyPart.service");
+const UserService = require("../../services/User.service");
+const HealthProfessionalTypeService = require("../../services/HealthProfessionalType.service");
 
 const healthProfessionalTypeService = new HealthProfessionalTypeService();
 const adminService = new AdminService();
-const userService = new UserService();;
+const userService = new UserService();
 const templateService = new TemplateService();
 const exerciseService = new ExerciseService();
 const bodyPartService = new BodyPartService();
-
-
 
 let admin = null;
 let admin_token = null;
@@ -31,10 +28,8 @@ let patient_token = null;
 let healthProfessional = null;
 let healthProfessional_token = null;
 
-
 beforeAll(async () => {
   await db.connect();
-  
 });
 
 afterAll(async () => {
@@ -45,101 +40,123 @@ afterAll(async () => {
 beforeEach(async () => {
   await db.clear();
 
-  const new_admin = { email: 'admin@email.com',photo: { key: "photo-key" }, name: 'Tester Admin', password: 'my_pass' };
-  const new_patient = { email: "test@email.com", name: "tester one", photo: { key: "photo-key" }, password: "my_pass", role: env.PATIENT_ROLE };
-	const new_healthProfessional = { email: "test1@email.com", name: "tester one", photo: { key: "photo-key" }, password: "my_pass", role: env.HEALTH_PROFESSIONAL_ROLE };
-	const new_healthProfessionalType = { name: "physio" }
- 
-  
-  healthProfessionalType = await healthProfessionalTypeService.create(new_healthProfessionalType);
-	patient = await userService.create(new_patient);
-	admin = await adminService.create(new_admin);
-	healthProfessional = await userService.create(new_healthProfessional);
+  const new_admin = {
+    email: "admin@email.com",
+    photo: { key: "photo-key" },
+    name: "Tester Admin",
+    password: "my_pass",
+  };
+  const new_patient = {
+    email: "test@email.com",
+    name: "tester one",
+    photo: { key: "photo-key" },
+    password: "my_pass",
+    role: env.PATIENT_ROLE,
+  };
+  const new_healthProfessional = {
+    email: "test1@email.com",
+    name: "tester one",
+    photo: { key: "photo-key" },
+    password: "my_pass",
+    role: env.HEALTH_PROFESSIONAL_ROLE,
+  };
+  const new_healthProfessionalType = { name: "physio" };
+
+  healthProfessionalType = await healthProfessionalTypeService.create(
+    new_healthProfessionalType
+  );
+  patient = await userService.create(new_patient);
+  admin = await adminService.create(new_admin);
+  healthProfessional = await userService.create(new_healthProfessional);
 
   const patient_body = {
-		email: patient.email,
-		_id: patient._id,
-		role: env.PATIENT_ROLE,
-		creator: patient.creator,
-	}
+    email: patient.email,
+    _id: patient._id,
+    role: env.PATIENT_ROLE,
+    creator: patient.creator,
+  };
 
-	const admin_body = {
-		email: admin.email,
-		_id: admin._id,
-		role: env.ADMIN_ROLE,
-	}
-	const healthProfessional_body = {
-		email: healthProfessional.email,
-		_id: healthProfessional._id,
-		role: env.HEALTH_PROFESSIONAL_ROLE,
-	}
+  const admin_body = {
+    email: admin.email,
+    _id: admin._id,
+    role: env.ADMIN_ROLE,
+  };
+  const healthProfessional_body = {
+    email: healthProfessional.email,
+    _id: healthProfessional._id,
+    role: env.HEALTH_PROFESSIONAL_ROLE,
+  };
 
   admin_token = AuthenticationManager.createToken(admin_body);
-	patient_token = AuthenticationManager.createToken(patient_body);
-	healthProfessional_token = AuthenticationManager.createToken(healthProfessional_body);
-  console.log("Admin" , admin_token)
+  patient_token = AuthenticationManager.createToken(patient_body);
+  healthProfessional_token = AuthenticationManager.createToken(
+    healthProfessional_body
+  );
+  console.log("Admin", admin_token);
   console.log("Patient token", patient_token);
-  console.log("HP TOKEN", healthProfessional_token)
+  console.log("HP TOKEN", healthProfessional_token);
 });
-describe('ExerciseTemplates Endpoints /api/templates/users/', () =>{
+describe("ExerciseTemplates Endpoints /api/templates/users/", () => {});
 
-})
-
-describe('ExerciseTemplate Endpoints /api/templates/', () => {
-  test('POST', async () => {
+describe("ExerciseTemplate Endpoints /api/templates/", () => {
+  test("POST", async () => {
     // Prepare data for the test
-    const tempBodyPart = { name: 'part1', description: 'desc' };
+    const tempBodyPart = { name: "part1", description: "desc" };
     const bodyPart = await bodyPartService.create(tempBodyPart);
 
     // console.log("bodypaert",bodyPart)
 
     const newExercise = {
-      title: 'Test Exercise',
-      description: 'This is the test description',
+      title: "Test Exercise",
+      description: "This is the test description",
       bodyPart: bodyPart._id,
       creator: {
-				userRole: env.ADMIN_ROLE,
-				admin: admin._id
-			},
-			lastEditor: {
-				userRole: env.ADMIN_ROLE,
-				admin: admin._id
-			},
-			video: {
-				url: "/video.mp4",
-				key: "test-video",
-			},
-			audio: {
-				url: "/audio.mp3",
-				key: "test-audio",
-			},
-			thumbImage: {
-				url: "/video.jpg",
-				key: "test-thumbImage",
-			},
-			image: {
-				url: "/image.jpg",
-				key: "test-image",
-			}
+        userRole: env.ADMIN_ROLE,
+        admin: admin._id,
+      },
+      lastEditor: {
+        userRole: env.ADMIN_ROLE,
+        admin: admin._id,
+      },
+      video: {
+        url: "/video.mp4",
+        key: "test-video",
+      },
+      audio: {
+        url: "/audio.mp3",
+        key: "test-audio",
+      },
+      thumbImage: {
+        url: "/video.jpg",
+        key: "test-thumbImage",
+      },
+      image: {
+        url: "/image.jpg",
+        key: "test-image",
+      },
     };
 
     const exercise = await exerciseService.create(newExercise);
     // console.log("newExercise",newExercise)
 
     const newTemplate = {
-      title: 'Test Template',
-      description: 'This is the test template description',
+      title: "Test Template",
+      description: "This is the test template description",
+      thumbImage: {
+        url: "/video.jpg",
+        key: "test-thumbImage",
+      },
       timestamp: new Date().getTime(),
       patient: patient._id,
-			healthProfessional: healthProfessional._id,
-			healthProfessionalType: healthProfessionalType._id,
+      healthProfessional: healthProfessional._id,
+      healthProfessionalType: healthProfessionalType._id,
       warmup: [
         {
           exercise: exercise._id.toString(),
           sets: 3,
           reps: 10,
           time: 30,
-          intensity: 'low',
+          intensity: "low",
         },
       ],
       main: [
@@ -148,7 +165,7 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
           sets: 2,
           reps: 8,
           time: 20,
-          intensity: 'high',
+          intensity: "high",
         },
       ],
       cooldown: [
@@ -157,7 +174,7 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
           sets: 2,
           reps: 8,
           time: 20,
-          intensity: 'high',
+          intensity: "high",
         },
       ],
       creator: {
@@ -169,25 +186,24 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
         admin: admin._id,
       },
     };
-    const template = await templateService.create(newTemplate)
-    // console.log("This is the created template",template)
+    const template = await templateService.create(newTemplate);
+    console.log("This is the created template", template);
     // Perform the API request
     return agent
-      .post('/api/templates/text')
+      .post("/api/templates/")
       .send(newTemplate)
-      .set('Auth-Token', admin_token)
-      .expect(404)
+      .set("Auth-Token", admin_token)
+      .expect(201)
       .then((res) => {
-        // console.log(res.body)
-        expect(res.body.body.title).toMatch(newTemplate.title);
-        expect(res.body.body.description).toMatch(newTemplate.description);
-        expect(res.body.body.warmup).toEqual(newTemplate.warmup)
-        expect(res.body.body.cooldown).toEqual(newTemplate.cooldown)
-        expect(res.body.body.main).toEqual(newTemplate.main)
+        console.log(res.body);
+        console.log(res.body);
+        expect(res.body.title).toMatch(template.title);
+        expect(res.body.description).toMatch(template.description);
+        expect(res.body.warmup).toEqual(template.warmup);
+        expect(res.body.cooldown).toEqual(template.cooldown);
+        expect(res.body.main).toEqual(template.main);
         // Add more assertions based on your model structure
-       
       });
-      
   });
   // test('GET /:id', async () => {
   //   // Assuming you have a function to create an ExerciseTemplate
@@ -195,34 +211,34 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //   const bodyPart = await bodyPartService.create(tempBodyPart);
   //   // console.log(bodyPart)
   //   const new_exercise = {
-	// 		title: "Test Exercise",
-	// 		description: "This is the test description",
-	// 		bodyPart: bodyPart._id,
-	// 		creator: {
-	// 			userRole: env.ADMIN_ROLE,
-	// 			admin: admin._id
-	// 		},
-	// 		lastEditor: {
-	// 			userRole: env.ADMIN_ROLE,
-	// 			admin: admin._id
-	// 		},
-	// 		video: {
-	// 			url: "/video.mp4",
-	// 			key: "test-video",
-	// 		},
-	// 		audio: {
-	// 			url: "/audio.mp3",
-	// 			key: "test-audio",
-	// 		},
-	// 		thumbImage: {
-	// 			url: "/video.jpg",
-	// 			key: "test-thumbImage",
-	// 		},
-	// 		image: {
-	// 			url: "/image.jpg",
-	// 			key: "test-image",
-	// 		}
-	// 	}
+  // 		title: "Test Exercise",
+  // 		description: "This is the test description",
+  // 		bodyPart: bodyPart._id,
+  // 		creator: {
+  // 			userRole: env.ADMIN_ROLE,
+  // 			admin: admin._id
+  // 		},
+  // 		lastEditor: {
+  // 			userRole: env.ADMIN_ROLE,
+  // 			admin: admin._id
+  // 		},
+  // 		video: {
+  // 			url: "/video.mp4",
+  // 			key: "test-video",
+  // 		},
+  // 		audio: {
+  // 			url: "/audio.mp3",
+  // 			key: "test-audio",
+  // 		},
+  // 		thumbImage: {
+  // 			url: "/video.jpg",
+  // 			key: "test-thumbImage",
+  // 		},
+  // 		image: {
+  // 			url: "/image.jpg",
+  // 			key: "test-image",
+  // 		}
+  // 	}
 
   //   const exercise = await exerciseService.create(new_exercise)
   //   console.log("exercise", exercise)
@@ -249,7 +265,7 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //     ],
   //     cooldown: [
   //       {
-  //         exercise:exercise._id.toString(), 
+  //         exercise:exercise._id.toString(),
   //         sets: 2,
   //         reps: 8,
   //         time: 20,
@@ -278,10 +294,10 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   // test('GET /:id', async () => {
   //   const tempBodyPart = { name: 'part1', description: 'desc' };
   //   const bodyPart = await bodyPartService.create(tempBodyPart);
-  
+
   //   const expectedExerciseCount = 3; // Adjust as needed
   //   const exercises = [];
-  
+
   //   for (let i = 0; i < expectedExerciseCount; i++) {
   //     const newExercise = {
   //       title: `Test Exercise ${i}`,
@@ -313,11 +329,11 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //       }
   //       // ... other exercise properties
   //     };
-  
+
   //     const exercise = await exerciseService.create(newExercise);
   //     exercises.push(exercise);
   //   }
-  
+
   //   const template = await exerciseTemplateService.create({
   //     title: 'Test Template',
   //     description: 'This is the test template description',
@@ -345,17 +361,17 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //     creator: { userRole: env.ADMIN_ROLE, admin: admin._id },
   //     lastEditor: { userRole: env.ADMIN_ROLE, admin: admin._id },
   //   });
-  
+
   //   return agent
   //     .get(`/api/templates/${template._id}`)
   //     .set('Auth-Token', admin_token)
   //     .expect(200)
   //     .then((res) => {
   //       const responseBody = res.body.body;
-  
+
   //       expect(responseBody.title).toEqual(template.title);
   //       expect(responseBody.description).toEqual(template.description);
-  
+
   //       // Loop through warmup exercises and perform assertions
   //       for (let i = 0; i < expectedExerciseCount; i++) {
   //         const expectedWarmupExercise = template.warmup[i];
@@ -391,64 +407,63 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //         expect(actualcooldownExercise.intensity).toEqual(expectedcooldownExercise.intensity);
   //         // Add more assertions based on your model structure
   //       }
-  
+
   //       // Perform similar assertions for main and cooldown exercises
   //     });
   // });
-  
 
   // test('GET /api/templates/', async () => {
   //   const temp = {
-	// 		name: "part1",
-	// 		description: "desc"
-	// 	}
+  // 		name: "part1",
+  // 		description: "desc"
+  // 	}
 
-	// 	const part = await bodyPartService.create(temp);
+  // 	const part = await bodyPartService.create(temp);
 
-	// 	const new_exercise = {
-	// 		title: "Test Exercise",
-	// 		description: "This is the test description",
-	// 		bodyPart: part._id,
-	// 		creator: {
-	// 			userRole: env.ADMIN_ROLE,
-	// 			admin: admin._id
-	// 		},
-	// 		lastEditor: {
-	// 			userRole: env.ADMIN_ROLE,
-	// 			admin: admin._id
-	// 		},
-	// 		video: {
-	// 			url: "/video.mp4",
-	// 			key: "test-video",
-	// 		},
-	// 		audio: {
-	// 			url: "/audio.mp3",
-	// 			key: "test-audio",
-	// 		},
-	// 		thumbImage: {
-	// 			url: "/video.jpg",
-	// 			key: "test-thumbImage",
-	// 		},
-	// 		image: {
-	// 			url: "/image.jpg",
-	// 			key: "test-image",
-	// 		}
-	// 	}
+  // 	const new_exercise = {
+  // 		title: "Test Exercise",
+  // 		description: "This is the test description",
+  // 		bodyPart: part._id,
+  // 		creator: {
+  // 			userRole: env.ADMIN_ROLE,
+  // 			admin: admin._id
+  // 		},
+  // 		lastEditor: {
+  // 			userRole: env.ADMIN_ROLE,
+  // 			admin: admin._id
+  // 		},
+  // 		video: {
+  // 			url: "/video.mp4",
+  // 			key: "test-video",
+  // 		},
+  // 		audio: {
+  // 			url: "/audio.mp3",
+  // 			key: "test-audio",
+  // 		},
+  // 		thumbImage: {
+  // 			url: "/video.jpg",
+  // 			key: "test-thumbImage",
+  // 		},
+  // 		image: {
+  // 			url: "/image.jpg",
+  // 			key: "test-image",
+  // 		}
+  // 	}
   //   //to create multiple exercises
   //   const expectedCount = 15;
-	// 	const exercises = [];
+  // 	const exercises = [];
 
-	// 	for (let i = 0; i < expectedCount; i++) {
-	// 		new_exercise.title = new_exercise.title + i;
-	// 		new_exercise.description = new_exercise.description + i;
-	// 		new_exercise.video.key = new_exercise.video.key + i;
-	// 		new_exercise.image.key = new_exercise.image.key + i;
-	// 		new_exercise.audio.key = new_exercise.audio.key + i;
-	// 		new_exercise.thumbImage.key = new_exercise.thumbImage.key + i;
+  // 	for (let i = 0; i < expectedCount; i++) {
+  // 		new_exercise.title = new_exercise.title + i;
+  // 		new_exercise.description = new_exercise.description + i;
+  // 		new_exercise.video.key = new_exercise.video.key + i;
+  // 		new_exercise.image.key = new_exercise.image.key + i;
+  // 		new_exercise.audio.key = new_exercise.audio.key + i;
+  // 		new_exercise.thumbImage.key = new_exercise.thumbImage.key + i;
 
-	// 		const exercise = await exerciseService.create(new_exercise);
-	// 		exercises.push(exercise);
-	// 	}
+  // 		const exercise = await exerciseService.create(new_exercise);
+  // 		exercises.push(exercise);
+  // 	}
 
   //   // Assuming you have a function to create multiple ExerciseTemplates
   //   const templates = await Promise.all([
@@ -519,12 +534,5 @@ describe('ExerciseTemplate Endpoints /api/templates/', () => {
   //     });
   // });
 
-
   // Add more test cases for other endpoints (GET, PUT, DELETE) and edge cases as needed
 });
-
-
-
-
-
-
